afbf0aa34a44590800996f76ecbd3d41a3ef47c5
SourceSafeUser Kclark <SourceSafeUser Kclark>
2014-09-21 11:43:41 -0400

    {8/4/2005 3:16:00 AM}

--- clsFileListDB.vb
+++ clsFileListDB.vb
@@ -2,7 +2,26 @@ Imports System.IO
 Imports System.IO.Directory
 Imports System.IO.File
 Public Class clsFileListDB
+    Public Event List(ByVal Message As String)
+    Private mCancel As Boolean = False
+    Private mCount As Long
     Private mSQLConnect As SqlClient.SqlConnection
+    Public Property Cancel() As Boolean
+        Get
+            Return mCancel
+        End Get
+        Set(ByVal Value As Boolean)
+            mCancel = Value
+        End Set
+    End Property
+    Public Property Count() As Long
+        Get
+            Return mCount
+        End Get
+        Set(ByVal Value As Long)
+            mCount = Value
+        End Set
+    End Property
     Public Property SQLConnect() As SqlClient.SqlConnection
         Get
             Return mSQLConnect
@@ -45,18 +64,21 @@ Public Class clsFileListDB
         Try
             Dim diList As DirectoryInfo() = BaseDir.GetDirectories()
             For Each di As DirectoryInfo In diList
-                Dim ValueList() As String = { _
-                    Replace(di.FullName, "'", "''"), _
-                    di.Attributes.ToString, _
-                    di.CreationTime.ToString, _
-                    di.LastAccessTime.ToString, _
-                    di.LastWriteTime.ToString}
-                SQLSource = String.Format("'{0}',0,'{1}','{2}','{3}','{4}'", ValueList)
-                SQLSource = String.Format("INSERT INTO FileList ({0}) VALUES ({1})", ColumnList, SQLSource)
-                Me.DoCommand(SQLSource)
                 Select Case di.Name
+                    Case "System Volume Information"
                     Case "Temporary Internet Files"
                     Case Else
+                        Dim ValueList() As String = { _
+                            Replace(di.FullName, "'", "''"), _
+                            di.Attributes.ToString, _
+                            IIf(di.CreationTime >= CDate("01/01/1753"), di.CreationTime.ToString, "NULL"), _
+                            IIf(di.LastAccessTime >= CDate("01/01/1753"), di.LastAccessTime.ToString, "NULL"), _
+                            IIf(di.LastWriteTime >= CDate("01/01/1753"), di.LastWriteTime.ToString, "NULL")}
+                        SQLSource = String.Format("'{0}',0,'{1}','{2}','{3}','{4}'", ValueList)
+                        SQLSource = String.Format("INSERT INTO FileList ({0}) VALUES ({1})", ColumnList, SQLSource)
+                        Me.DoCommand(SQLSource)
+                        RaiseEvent List(di.FullName)
+                        If mCancel Then Exit Try
                         ListFiles(di)
                 End Select
             Next
@@ -66,12 +88,14 @@ Public Class clsFileListDB
                     Replace(fi.FullName, "'", "''"), _
                     fi.Length.ToString, _
                     fi.Attributes.ToString, _
-                    fi.CreationTime.ToString, _
-                    fi.LastAccessTime.ToString, _
-                    fi.LastWriteTime.ToString}
+                    IIf(fi.CreationTime >= CDate("01/01/1753"), fi.CreationTime.ToString, "NULL"), _
+                    IIf(fi.LastAccessTime >= CDate("01/01/1753"), fi.LastAccessTime.ToString, "NULL"), _
+                    IIf(fi.LastWriteTime >= CDate("01/01/1753"), fi.LastWriteTime.ToString, "NULL")}
                 SQLSource = String.Format("'{0}',{1},'{2}','{3}','{4}','{5}'", ValueList)
                 SQLSource = String.Format("INSERT INTO FileList ({0}) VALUES ({1})", ColumnList, SQLSource)
                 Me.DoCommand(SQLSource)
+                RaiseEvent List(fi.FullName)
+                If mCancel Then Exit Try
             Next
         Catch ex As Exception
             MessageBox.Show(ex.ToString, "FileListDB.ListFiles", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification)
@@ -83,8 +107,12 @@ Public Class clsFileListDB
     End Sub
     Private Overloads Shared Function Main(ByVal args() As String) As Integer
         Dim fl As New clsFileListDB
+        Dim iCount As Long
         Dim SQLSource As String
         Try
+            Application.EnableVisualStyles()
+            Application.DoEvents()
+
             If args.Length < 3 Then
                 Dim Message As String = _
                     "FileList Options:" & vbCrLf & _
@@ -111,6 +139,18 @@ Public Class clsFileListDB
             fl.SQLConnect.Open()
 
             Try
+                Dim SQLCommand As New SqlClient.SqlCommand
+                SQLSource = "Select Count(*) From FileList"
+                With SQLCommand
+                    .CommandText = SQLSource
+                    .CommandType = CommandType.Text
+                    .Connection = fl.SQLConnect
+                    fl.Count = .ExecuteScalar
+                End With
+            Catch ex As Exception
+            End Try
+
+            Try
                 SQLSource = "DROP TABLE FileList"
                 fl.DoCommand(SQLSource)
             Catch ex As Exception
@@ -121,22 +161,27 @@ Public Class clsFileListDB
             SQLSource &= "[Path] [nvarchar] (1024) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,"
             SQLSource &= "[Size] [bigint] NULL,"
             SQLSource &= "[Attributes] [nvarchar] (256) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,"
-            SQLSource &= "[CreationTime] [datetime] NOT NULL,"
-            SQLSource &= "[LastAccessTime] [datetime] NOT NULL,"
-            SQLSource &= "[LastWriteTime] [datetime] NOT NULL"
+            SQLSource &= "[CreationTime] [datetime] NULL,"
+            SQLSource &= "[LastAccessTime] [datetime] NULL,"
+            SQLSource &= "[LastWriteTime] [datetime] NULL"
             SQLSource &= ") ON [PRIMARY]"
             fl.DoCommand(SQLSource)
 
-            'Dim cntFolders As Long = 0
-            'Dim cntFiles As Long = 0
-            'fl.CountFiles(Root, cntFolders, cntFiles)
-            ''Use these stats to display a window with a progress bar...
-            'Dim frm As New frmStats
-            'frm.ShowDialog()
-            fl.ListFiles(Root)
+            If fl.Count > 0 Then
+                'Use these stats to display a window with a progress bar...
+                Dim frm As New frmProgress(fl)
+                With frm
+                    .Text = String.Format("FileListDB - Listing Files From {0} into {1}", args(1), args(2))
+                    .Show()
+                    fl.ListFiles(Root)
+                    .Close()
+                End With
+            Else
+                fl.ListFiles(Root)
+            End If
             Return 0
         Catch ex As Exception
-                MessageBox.Show(ex.ToString, "FileListDB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification)
+            MessageBox.Show(ex.ToString, "FileListDB", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification)
             Return 1
         Finally
         End Try
